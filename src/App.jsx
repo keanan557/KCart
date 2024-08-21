0d18cd890946e0e5c6ed19087e8f46a1cfa60e95
Keanan557 <keananwork21@gmail.com>
2024-08-21 12:33:03 +0100

    comment

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Navbar } from "./components/navbar.jsx"; 
+import {Shop} from './pages/shop/shop'
+import {Cart} from './pages/cart/cart'
+import { ShopContextProvider } from "./context/shop-context.jsx";
+
+export default function App(){
+    return <div className="App">
+        <ShopContextProvider>
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Shop />}/>
+                <Route path="/cart" element={<Cart />}/>
+            </Routes>
+        </Router>
+        </ShopContextProvider>
+    </div>
+}
+
---
+++ src/products.js
@@ -0,0 +1,31 @@
+import ps5 from "./images/ps5.png"
+import xbox from "./images/xbox.png"
+import laptop from "./images/lapop.png"
+import nswitch from "./images/switch.png"
+
+export const PRODUCTS = [
+    {
+        id:1,
+        productName: "PS5",
+        price: 14000,
+        productImage: ps5,
+    },
+    {
+        id:2,
+        productName: "xbox",
+        price: 13000,
+        productImage: xbox,
+    },
+    {
+        id:3,
+        productName: "laptop",
+        price: 12500,
+        productImage: laptop,
+    },
+    {
+        id:4,
+        productName: "switch",
+        price: 14000,
+        productImage: nswitch,
+    },
+]
